9ed83b085ae95873b1b0e8fb90cde02114bbaa6c
leekichang <kichang.lee@yonsei.ac.kr>
2023-04-20 22:11:40 +0900

    initial commit

---
+++ client.py
@@ -0,0 +1,63 @@
+#client.py
+import time
+from socket import *
+import pickle
+import numpy as np
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Socket Programming')
+    parser.add_argument('--ip', default='127.0.0.1', type=str)
+    parser.add_argument('--p', default=9784, type=int)
+    args = parser.parse_args()
+    return args
+
+class Client:
+    def __init__(self, SERVER_ADDRESS):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect(SERVER_ADDRESS)
+        self.connected = True
+        print("Client Connected!")
+
+    def send(self):
+        data = int(input('>>>'))
+        self.socket.sendall(pickle.dumps(data))
+        print('전송완료')
+        if data == 0:
+            print("연결 종료")
+            self.connected = False
+            self.socket.close()
+
+    def recv(self):
+        data_total_len     = int(self.socket.recv(1024))
+        left_recv_len      = data_total_len
+        buffer_size        = data_total_len
+        time.sleep(1)
+
+        recv_data = []
+        while True:
+            chunk = self.socket.recv(buffer_size)
+            recv_data.append(chunk)
+            left_recv_len -= len(chunk)
+            if left_recv_len <= 0:
+                break
+        if not left_recv_len == 0:
+            print("Packet Loss!")
+        else:
+            print(f'받은 데이터:{pickle.loads(b"".join(recv_data))}\n\n{data_total_len}')
+    
+    def run(self):
+        while True:
+            self.send()
+            if not self.check_connection():break
+            self.recv()
+            if not self.check_connection():break
+
+    def check_connection(self):
+        return self.connected
+
+if __name__ == '__main__':
+    args = parse_args()
+    SERVER_ADDRESS = (args.ip, args.p)
+    client = Client(SERVER_ADDRESS)
+    client.run()
---
+++ server.py
@@ -0,0 +1,72 @@
+#server.py
+import time
+from socket import *
+import pickle
+import numpy as np
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Socket Programming')
+    parser.add_argument('--p', default=9784, type=int)
+    args = parser.parse_args()
+    return args
+
+class Server:
+    def __init__(self, SERVER_ADDRESS):
+        self.address  = SERVER_ADDRESS
+        self.socket   = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind(self.address)
+        self.socket.listen(1)
+        print("server is ready")
+        self.client_socket, self.client_addr = self.socket.accept()
+        print(f"server connected by {self.client_addr}")
+        self.request = None
+        self.connected = True
+
+    def send(self):
+        if self.request != None and self.request != 0:
+            data = np.random.rand(self.request)
+            data_byte = pickle.dumps(data)
+            self.client_socket.sendall(str(len(data_byte)).encode())
+            time.sleep(1)
+
+            self.client_socket.sendall(data_byte)
+            print(f"DATA SENT")
+            print(f"np.shape(data_byte):{np.shape(data_byte)}")
+            print(f"len(data_byte):{len(data_byte)}")
+            self.request = None
+        
+    def recv(self):
+        recv_data = []
+        while True:
+            chunk = self.client_socket.recv(4096)
+            recv_data.append(chunk)
+            if len(chunk) < 4096:
+                break
+        data = pickle.loads(b''.join(recv_data))
+        if data == 0:
+            self.client_socket.close()
+            self.connected = False
+            print("DISCONNECTED")
+            self.connect()
+        
+        print(f"RECEIVCED DATA: {data}")
+        self.request = data
+        
+    def connect(self):
+        print("WAITING FOR NEW CONNECTION")
+        self.socket.listen(1)
+        self.client_socket, self.client_addr = self.socket.accept()
+        print(f"server connected by {self.client_addr}")
+        self.is_connected = True
+    
+    def run(self):
+        while True:
+            self.recv()
+            self.send()
+
+if __name__ == '__main__':
+    args = parse_args()
+    SERVER_ADDRESS = ('0.0.0.0', args.p)
+    server = Server(SERVER_ADDRESS)
+    server.run()
